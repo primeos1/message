53e9ee92b9dcd02f735c324d34f021771158ba5f
Miley 6th <akhenisio@yahoo.com>
2024-01-05 15:21:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yay, see you on the 13th!";
+
+  // Redirect to another page
+  window.location.href = "flower.html";
+
+  // If you want to open the link in a new tab, you can use:
+  // window.open("https://example.com/another-page", "_blank");
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
